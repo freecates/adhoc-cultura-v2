c35f5fd70e9be85e586bb262c58bf06bfa9104b0
Ramon <ramongil@ramongil.cat>
2024-01-11 18:37:15 +0100

    feat: [main] Added new page serveis and its dynamic routes

---
+++ src/app/serveis/page.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-unescaped-entities */
+import api from "@/lib/api"
+import SectionGripOfCards from "@/components/sectionGridOfCards"
+
+type Servei = {
+  title: string;
+  name: string;
+};
+type ServeisProps = { 
+  serveis: Servei[];
+};
+
+export default async function Serveis(): Promise<JSX.Element> {
+  const { serveis }: ServeisProps = await getData();
+  return (
+      <main className="flex-1">
+        <SectionGripOfCards title={"Serveis"} description={"A partir de la nostra metodologia i gràcies als nostres partners podem oferir un ampli ventall de serveis i adaptar-nos a les necessitats dels nostres clients i col·laboradors per a poder desenvolupar projectes per promocionar la cultura i apropar-la a la societat."} data={serveis} image={{src: "/bg-adhoc-cultura-320.jpg", alt: "Adhoc Logo"}} />
+      </main>
+  )
+}
+
+const getData = async (): Promise<any> => {
+  const [serveis] = await Promise.all([
+      api.adhocCulturaData.getData('serveis'),
+  ]);
+  return {
+      serveis,
+  };
+};
--- src/components/sectionGridOfCards.tsx
+++ src/components/sectionGridOfCards.tsx
@@ -1,9 +1,12 @@
 import { CardHeader, CardContent, Card } from "@/components/ui/card"
 import Image from "next/image"
+import Link from "next/link";
 
 type DataObject = {
     title: string;
     name: string;
+    slug?: string;
+    type?: string;
   };
 
 type Grid = {
@@ -23,18 +26,35 @@ const GridOfCards: React.FC<Grid> = ({ data, image }) => (
     <div className="mx-auto grid items-start gap-8 sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl lg:grid-cols-3">
         {data.map((d, index): JSX.Element => (
         <Card key={index + d.title}>
-            <CardHeader>
-                {image && <Image src={image.src} alt={image.alt} width={48} height={48} />}
-                <h3 className="text-lg font-bold">{d.title}</h3>
-            </CardHeader>
-            <CardContent>
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                    {d.name}
-                </p>
-            </CardContent>
+            {d.slug ? 
+                <Link href={`/${d.type ? `${d.type}/` : ''}${d.slug}`}>
+                    <CardHeader>
+                        {image && <Image src={image.src} alt={image.alt} width={48} height={48} />}
+                        <h3 className="text-lg font-bold">{d.title}</h3>
+                    </CardHeader>
+                    <CardContent>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">
+                            {d.name}
+                        </p>
+                    </CardContent>
+                </Link>: 
+                <>
+                    <CardHeader>
+                        {image && <Image src={image.src} alt={image.alt} width={48} height={48} />}
+                        <h3 className="text-lg font-bold">{d.title}</h3>
+                    </CardHeader>
+                    <CardContent>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">
+                            {d.name}
+                        </p>
+                    </CardContent>
+                </>
+            }
         </Card>
         ))}
     </div>)
+
+export { GridOfCards };
 
 const SectionGripOfCards: React.FC<ISectionProps> = ({ title, description, data, image }) => {
   return (
@@ -42,9 +62,11 @@ const SectionGripOfCards: React.FC<ISectionProps> = ({ title, description, data,
         <div className="container mx-auto space-y-12 px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">{title}</h2>
-                <p className="max-w-[900px] text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                {description}
-                </p>
+                {description &&
+                    <p className="max-w-[900px] text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                    {description}
+                    </p>
+                }
             </div>
             <GridOfCards data={data} image={image} />
         </div>
