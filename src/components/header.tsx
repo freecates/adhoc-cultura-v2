d91d1d3ce35530d49f4552ecae42d4131d9b318e
Ramon <ramongil@ramongil.cat>
2024-01-17 22:23:17 +0100

    chore: [main] Added route icon to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import dynamicIconImports from "lucide-react/dynamicIconImports";
 import { Button } from "./ui/button";
 import Image from "next/image";
 import api from "@/lib/api";
+import Icon from "./ui/icon";
 
 type Route = {
   name: string;
   url: string;
+  icon?: {
+      name: keyof typeof dynamicIconImports;
+      color?: string;
+      size?: string;
+  };
 };
 type RoutesProps = { 
   routes: Route[]
@@ -13,6 +20,8 @@ type RoutesProps = {
 
 export default async function Header(): Promise<JSX.Element> {
   const { routes }: RoutesProps = await getData();
+  const textRoutes = routes.filter((r) => r.icon === undefined);
+  const iconRoutes = routes.find((r) => r.icon);
     return (
         
         <header className="px-4 lg:px-6 py-2 md:h-20 flex flex-col md:flex-row items-center border-b">
@@ -25,12 +34,16 @@ export default async function Header(): Promise<JSX.Element> {
               src="/bg-adhoc-cultura-320.jpg"
             />
         </Link>
-        <nav className="my-2 md:my-0 md:ml-auto md:mr-2 flex items-center justify-center gap-4 sm:gap-6">
-        {routes.map((r, index): JSX.Element => (
+        <nav className="my-2 md:my-0 md:ml-auto md:mr-4 flex items-center justify-center gap-4 sm:gap-6">
+        {textRoutes.map((r, index): JSX.Element => (
             <Link key={index + r.name} className="text-sm font-medium hover:underline underline-offset-4" href={r.url}>
               {r.name}
             </Link>
           ))}
+        {iconRoutes && iconRoutes.icon &&
+          <Link title={iconRoutes.name} href={iconRoutes.url}>
+            <Icon name={iconRoutes.icon.name} color={iconRoutes.icon?.color} size={iconRoutes.icon?.size} />
+          </Link>}
         </nav>
         <div className="hidden md:block">
           <Button asChild={true} variant={'secondary'} size={"sm"}><a href="https://www.aracultura.com/" target="_blank">ARA Cultura</a></Button>
