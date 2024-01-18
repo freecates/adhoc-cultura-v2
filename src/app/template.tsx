b6525257a5e7ff5bae67ccd4e028d84ba03740a3
Ramon <ramongil@ramongil.cat>
2024-01-18 18:04:17 +0100

    feat: [with-page-transitions] Added framer motion transitions to route pages

---
+++ src/app/template.tsx
@@ -0,0 +1,10 @@
+import Animate from "@/components/animate"
+
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+      <Animate>
+        {children}
+      </Animate>
+  )
+}
