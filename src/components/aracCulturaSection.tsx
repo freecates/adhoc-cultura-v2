671f3c21185d803d9179528cf4b667c1eb4d9532
Ramon <ramongil@ramongil.cat>
2024-01-19 11:21:02 +0100

    fix: [main] Added padding to section

--- src/components/aracCulturaSection.tsx
+++ src/components/aracCulturaSection.tsx
@@ -10,7 +10,7 @@ export default async function AraCulturaSection() {
     const { mdAraCultura }: AraCultura = await getData();
     return (
         <div>
-            <section className='w-full py-4 md:py-8 lg:py-12 bg-gray-950 text-white'>
+            <section className='w-full py-4 px-4 md:py-8 lg:py-12 bg-gray-950 text-white'>
                 <div className='container mx-auto'>
                     <div className='text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400'>
                         <MdFileContent content={mdAraCultura} />
