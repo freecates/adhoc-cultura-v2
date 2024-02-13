a38d73fe13bf6eed5004c0c25036d58fa77d2239
Ramon <ramongil@ramongil.cat>
2024-02-13 12:59:10 +0100

    chore: [main] Added asset adhoc creant vincle to adhoc page

--- src/app/(static)/adhoc/page.tsx
+++ src/app/(static)/adhoc/page.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 import QuotesPage from "@/components/quotesPage";
 import api from "@/lib/api";
+import { shimmer, toBase64 } from "@/lib/utils";
+import Image from "next/image";
 type Quote = {
     quote: string;
     author: string;
@@ -14,6 +16,19 @@ export default async function Adhoc(): Promise<JSX.Element> {
     return (
         <main className='flex-1'>
             <QuotesPage data={adhoc} />
+
+            <section className={`bg-white text-black w-full py-12`}>
+                <div className='mx-auto text-center'>
+                    <Image
+                        className='mx-auto aspect-[16/9] overflow-hidden object-cover transition-all duration-500 ease-in-out'
+                        alt={'Adhoc Creant Vincle'}
+                        src={'https://adhocc-data.vercel.app/static/adhoc/adhoc-creant-vincle.jpg'}
+                        height={933}
+                        width={1680}
+                        placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(1680, 933))}`}
+                    />
+                </div>
+            </section>
         </main>
     );
 }
