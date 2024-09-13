976b02ee6eef101bece9422beac9254995487406
Ramon <ramongil@ramongil.cat>
2024-09-13 13:44:55 +0200

    fix(team): [main] Fixed width

--- src/components/sectionPageHeader.tsx
+++ src/components/sectionPageHeader.tsx
@@ -44,7 +44,7 @@ const SectionPageHeader: React.FC<{
     const border = withBorder ? 'border-b' : '';
     return (
         <section className={`w-full pt-12 md:pt-24 lg:pt-32 ${border}`}>
-            <div className='px-4 md:px-6 space-y-10 xl:space-y-16'>
+            <div className='md:px-6 space-y-10 xl:space-y-16'>
                 <div className='grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16'>
                     <div>
                         {title && (
