d16c0b7286bbb77930d0713f35e73e4211f2fd06
Ramon <ramongil@ramongil.cat>
2025-11-13 12:57:25 +0100

    fix(ui): [with-next16-react-compiler-cache-components] Added suspense boundary

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,13 +1,10 @@
 import Animate from "@/components/animate"
-import { Suspense } from "react"
 
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
       <Animate>
-        <Suspense fallback={<div style={{ width: '100vw', height: '100vh' }}/>}>
           {children}
-        </Suspense>
       </Animate>
   )
 }
--- src/components/animate.tsx
+++ src/components/animate.tsx
@@ -1,15 +1,18 @@
-'use client'
+'use client';
 
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
+import { Suspense } from 'react';
 
 export default function Animate({ children }: { children: React.ReactNode }) {
-  return (
-    <motion.div
-      initial={{ y: 20, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ ease: 'easeInOut', duration: 1 }}
-    >
-      {children}
-    </motion.div>
-  )
+    return (
+        <motion.div
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ ease: 'easeInOut', duration: 1 }}
+        >
+            <Suspense fallback={<div style={{ width: '100vw', height: '100vh' }} />}>
+                {children}
+            </Suspense>
+        </motion.div>
+    );
 }
