edb7e2f46715085666c8be44e240b433651726cc
Ramon <ramongil@ramongil.cat>
2025-11-13 12:54:41 +0100

    fix(ui): [with-next16-react-compiler-cache-components] Added suspense boundary

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,10 +1,13 @@
 import Animate from "@/components/animate"
+import { Suspense } from "react"
 
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
       <Animate>
-        {children}
+        <Suspense fallback={<div style={{ width: '100vw', height: '100vh' }}/>}>
+          {children}
+        </Suspense>
       </Animate>
   )
 }
